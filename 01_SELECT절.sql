65d597320126443bcc3637a29d2c95551104c7d0
sabeom <tkqja717@naver.com>
2024-03-08 18:09:41 +0900

    sql-basic-01

---
+++ 01_SELECT절.sql
@@ -0,0 +1,67 @@
+
+-- 오라클의 한 줄 주석입니다.
+
+/*
+여러줄 주석입니다.
+ㅋㅋ
+*/
+
+-- SELECT [컬럼명(여러 개 가능)] FROM [테이블 이름]
+SELECT 
+    *
+FROM 
+    employees;
+-- select * from employees; 이렇게도 가능함
+
+SELECT
+    employee_id,
+    first_name,
+    last_name
+FROM
+    employees;
+    
+SELECT email, phone_number, hire_date
+FROM employees;
+
+-- 컬럼을 조회하는 위치에서 [ * / + - ] 연산이 가능합니다.
+
+SELECT
+    employee_id,
+    first_name,
+    last_name,
+    salary + salary*0.1 AS 성과금
+FROM employees;    
+
+-- NULL 값의 확인 (숫자 0이나 공백과는 다른 존재입니다.)
+SELECT department_id, commission_pct
+FROM employees;
+
+-- alias (컬럼명, 테이블명의 이름을 변경해서 조회합니다.)
+SELECT
+    first_name AS 이름,
+    last_name AS 성,
+    salary*300 AS 희망급여
+FROM employees;
+
+/*
+오라클은 홑따옴표로 문자를 표현가고, 문자열 안에 홑따옴표 특수기호를
+표현하고 싶다면 ''를 두 번 연속으로 쓰시면 됩니다.
+문자열 연결기호는 || 입니다. 문자열의 덧셈 연산을 허용하지 않습니다.
+*/
+SELECT
+    first_name || ' ' || last_name || '''s salary is $' || salary AS full_name
+FROM employees;
+
+-- DISTINCT (중복 행의 제거)
+SELECT department_id FROM employees;
+SELECT DISTINCT department_id FROM employees;
+
+-- ROWNUM, ROWID
+-- (로우넘: 쿼리에 의해 반환되는 행 번호를 반환)
+-- (로우아이디: 데이터베이스 내의 행의 주소를 반환)
+SELECT ROWNUM, ROWID, employee_id
+FROM employees;
+
+
+
+
---
+++ 02_WHERE절.sql
@@ -0,0 +1,27 @@
+
+SELECT * FROM employees;
+
+-- WHERE절 비교 (데이터 값은 대/소문자를 구분합니다.)
+SELECT first_name, last_name, job_id
+FROM employees
+WHERE job_id = 'it_prog';
+
+SELECT *
+FROM employees
+WHERE last_name = 'King';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
